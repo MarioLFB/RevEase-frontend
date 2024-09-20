34156b481f80f502956f71eece080d73f24d7fb9
Mario Borges <popesco19@hotmail.com>
2024-09-20 18:12:46 +0100

    Refactor login endpoint URL in auth service

--- revease/src/services/auth.js
+++ revease/src/services/auth.js
@@ -6,7 +6,7 @@ const api = axios.create({
 
 export const login = async (username, password) => {
     try {
-        const response = await api.post('api-token-auth/', {username, password});
+        const response = await api.post('api/auth/login/', {username, password});
 
         localStorage.setItem('token', response.data.token);
         return response.data;
