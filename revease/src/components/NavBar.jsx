02bc55f048ba18a319d37a56c201111c2cc99cb2
Mario Borges <popesco19@hotmail.com>
2024-09-27 19:39:58 +0100

    Refactor NavBar component to use react-bootstrap for styling and layout

--- revease/src/components/NavBar.jsx
+++ revease/src/components/NavBar.jsx
@@ -1,28 +1,49 @@
 import React, { useContext } from 'react';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import Login from './login';
 import LogoutButton from './Logout';
-import { AuthContext } from '../context/AuthContext'; 
+import { AuthContext } from '../context/AuthContext';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../assets/styles/components/Navigation.css';
 
 function Navigation() {
   const { user, loading } = useContext(AuthContext);
 
   if (loading) {
-    return <div>Loading...</div>;
+    return <div className="text-center">Loading...</div>;
   }
 
   return (
-    <Navbar className="bg-body-tertiary">
+    <Navbar expand="lg" className="navbar-transparent shadow-sm">
       <Container>
-        <Navbar.Brand href="#home">
-          React Bootstrap
+        <Navbar.Brand href="#home" className="fw-bold fs-4">
+          MyApp
         </Navbar.Brand>
-        {user ? ( 
-          <LogoutButton />
-        ) : (
-          <Login />
-        )}
+        <Navbar.Toggle aria-controls="navbar-nav" />
+        <Navbar.Collapse id="navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home" className="fs-5">Home</Nav.Link>
+            <Nav.Link href="#about" className="fs-5">About</Nav.Link>
+            <Nav.Link href="#services" className="fs-5">Services</Nav.Link>
+            <NavDropdown title="More" id="nav-dropdown" className="fs-5">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something else here</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            {user ? (
+              <LogoutButton className="btn btn-outline-dark me-2" />
+            ) : (
+              <Login className="btn btn-outline-dark me-2" />
+            )}
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
