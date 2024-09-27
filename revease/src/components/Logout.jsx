887e55a5af8168c2c2db52da3fb342e37d46061e
Mario Borges <popesco19@hotmail.com>
2024-09-27 20:02:41 +0100

    Refactor Logout component to use react-bootstrap for styling and layout

--- revease/src/components/Logout.jsx
+++ revease/src/components/Logout.jsx
@@ -1,14 +1,19 @@
 import React, { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
+import '../assets/styles/components/Logout.css';
 
 const LogoutButton = () => {
-  const { logout } = useContext(AuthContext); 
+  const { logout } = useContext(AuthContext);
 
   const handleLogout = () => {
     logout();
   };
 
-  return <button onClick={handleLogout}>Logout</button>;
+  return (
+    <button onClick={handleLogout} className="btn btn-logout btn-wide">
+      Logout
+    </button>
+  );
 };
 
 export default LogoutButton;
